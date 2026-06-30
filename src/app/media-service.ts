bd862124213b2fbe4f8802986eddead280d58c40
parista_k <parista_k@f112.ub.lan>
2026-06-30 11:04:58 +0800

    Feat: Implement media service and env files

--- src/app/media-service.ts
+++ src/app/media-service.ts
@@ -1,6 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class MediaService {}
+export class MediaService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:4700/api/media';
+
+  mediaList = signal<any[]>([]);
+
+  fetchMedia() {
+    this.http.get<any[]>(this.apiUrl).subscribe((data) => {
+      this.mediaList.set(data);
+    });
+  }
+
+  saveMedia(data: any) {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  updateMedia(id: string, data: any) {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
+  }
+
+  deleteMedia(id: string) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
