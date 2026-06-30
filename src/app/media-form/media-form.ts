cb7308ca4f907c4656514c398c7603fd99af5a1b
parista_k <parista_k@f112.ub.lan>
2026-06-30 10:22:05 +0800

    Feat: Added Media service and component, also set up Express server and Mongodb connection

---
+++ src/app/media-form/media-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-form',
+  imports: [],
+  templateUrl: './media-form.html',
+  styleUrl: './media-form.css',
+})
+export class MediaForm {}
